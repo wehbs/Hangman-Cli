9d2340526cf8814b58b5d8598a1d4e7aab0b7cee
wehbs <shamirwehbe@me.com>
2017-10-28 23:55:05 -0400

    some touch ups need to sperate files

--- index.js
+++ index.js
@@ -16,19 +16,22 @@ var winCount = 0;
 var guessesLeft;
 
 
-
-
 // WORD CONSTRUCTOR
 function Word(word) {
     this.word = word;
-    this.roundComplete = function () {
+};
 
+Word.prototype.roundComplete = function () {
+
+    // console.log('\n');
+    // console.log("Remaining Guesses  " + guessesLeft);
+    // FIGLET FUNCTION
+    figlet(blanksAndSuccesses.join(" "), function (err, data) {
         console.log('\n');
         console.log("Remaining Guesses  " + guessesLeft);
-        console.log("Animal To Guess  " + blanksAndSuccesses.join(" "));
         console.log("Guessed Wrong:  " + wrongLetters.join(" "));
+        console.log(data)
         console.log('\n');
-
 
         // Check if user won then add 1 to win count and cnosole.log(you won).
         if (lettersinWord.toString() == blanksAndSuccesses.toString()) {
@@ -56,45 +59,41 @@ function Word(word) {
             setTimeout(startGame, 200);
 
         }
-
-    }
-
+    });
 };
-
 
 
 // LETTER CONSTRUCTOR
 function Letter(letter) {
     this.letter = letter;
-    // Checks whether the letter selected macthes any letters of the chosen word.
-    this.checkLetters = function (letter) {
-        var isLetterInWord = false;
-        for (var i = 0; i < numBlanks; i++) {
-            if (selectedWord[i] == letter) {
-                isLetterInWord = true;
-            }
-        }
-        // If the letter is in the word replace the "_" location with the correct letter.
-        if (isLetterInWord) {
-            for (var i = 0; i < numBlanks; i++) {
-                if (selectedWord[i] == letter) {
-                    blanksAndSuccesses[i] = letter;
-                }
-            }
-
-        }
-        // If the letter is not in the word push it to the array of wrong letters and decrease the guessesleft variable by one.
-        else {
-            wrongLetters.push(letter);
-            guessesLeft--
-            console.log(guessesLeft);
-
-        }
-        setTimeout(startInquirer, 250);
-    };
 };
 
+// Checks whether the letter selected macthes any letters of the chosen word.
+Letter.prototype.checkLetters = function (letter) {
+    var isLetterInWord = false;
+    for (var i = 0; i < numBlanks; i++) {
+        if (selectedWord[i] == letter) {
+            isLetterInWord = true;
+        }
+    }
+    // If the letter is in the word replace the "_" location with the correct letter.
+    if (isLetterInWord) {
+        for (var i = 0; i < numBlanks; i++) {
+            if (selectedWord[i] == letter) {
+                blanksAndSuccesses[i] = letter;
+            }
+        }
 
+    }
+    // If the letter is not in the word push it to the array of wrong letters and decrease the guessesleft variable by one.
+    else {
+        wrongLetters.push(letter);
+        guessesLeft--
+        console.log(guessesLeft);
+
+    }
+    setTimeout(startInquirer, 250);
+};
 
 
 function startGame() {
@@ -104,7 +103,6 @@ function startGame() {
     lettersinWord = selectedWord.split("");
     numBlanks = lettersinWord.length;
 
-
     guessesLeft = 9;
     wrongLetters = [];
     blanksAndSuccesses = [];
@@ -113,22 +111,17 @@ function startGame() {
         blanksAndSuccesses.push("_");
     }
 
-
-    console.log('\n');
-    // console.log(selectedWord);
-    // console.log("Remaining Guesses  " + guessesLeft);    
-    console.log("Animal To Guess  " + blanksAndSuccesses.join(" "));
-    // console.log("Guessed Wrong:  " + wrongLetters.join(" "));    
-    console.log("Wins  " + winCount);
-    console.log('\n');
-
-
     // FIGLET FUNCTION
     figlet("Animal  Hangman", function (err, data) {
         console.log(data)
+        // FIGLET FUNCTION
+        figlet(blanksAndSuccesses.join(" "), function (err, data) {
+            console.log('\n');
+            console.log("Wins  " + winCount);
+            console.log(data)
+            console.log('\n');
+        });
     });
-
-
 };
 
 
@@ -141,15 +134,11 @@ function startInquirer() {
         message: "Guess a letter"
     }]).then(function (answers) {
 
-
-
         var letterGuessed = new Letter(answers.letter);
 
         // Run the picked letter through the check letter function
         letterGuessed.checkLetters(letterGuessed.letter);
         randomWord.roundComplete();
-        // console.log(letterGuessed.letter);        
-        //startInquirer();
     });
 };
 
